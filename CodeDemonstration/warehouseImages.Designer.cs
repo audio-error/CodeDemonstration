11f1c2c65816143b65758e6e45568ff46b7e7662
Sean <samacmil@our.ecu.edu.au>
2023-05-15 01:20:53 +0800

    Added the warehouse functions. I feel tired

--- CodeDemonstration/warehouseImages.Designer.cs
+++ CodeDemonstration/warehouseImages.Designer.cs
@@ -29,11 +29,156 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(warehouseImages));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.imageWithDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.imageWithDetailsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageWithDetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageWithDetailsBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "table1.jpg");
+            this.imageList1.Images.SetKeyName(1, "table2.jpg");
+            this.imageList1.Images.SetKeyName(2, "table3.jpg");
+            this.imageList1.Images.SetKeyName(3, "table4.jpg");
+            this.imageList1.Images.SetKeyName(4, "table5.jpg");
+            this.imageList1.Images.SetKeyName(5, "table6.jpg");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CodeDemonstration.Resource1.table2;
+            this.pictureBox1.Location = new System.Drawing.Point(464, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 186);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(464, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(464, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "table 1",
+            "table 2",
+            "table 3",
+            "table 4",
+            "table 5",
+            "table 6"});
+            this.comboBox1.Location = new System.Drawing.Point(87, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // imageWithDetailsBindingSource
+            // 
+            this.imageWithDetailsBindingSource.DataSource = typeof(CodeDemonstration.ImageWithDetails);
+            // 
+            // imageWithDetailsBindingSource1
+            // 
+            this.imageWithDetailsBindingSource1.DataSource = typeof(CodeDemonstration.ImageWithDetails);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(598, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(435, 45);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "On this page you can select any table and view it\'s details and image.\r\nLINQ quer" +
+    "ies can be perfomed on abstract datatypes like images and even structs\r\n or clas" +
+    "ses.";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // warehouseImages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "warehouseImages";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageWithDetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageWithDetailsBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ImageList imageList1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private ContextMenuStrip contextMenuStrip1;
+        private ComboBox comboBox1;
+        private BindingSource imageWithDetailsBindingSource;
+        private BindingSource imageWithDetailsBindingSource1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
